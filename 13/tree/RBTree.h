ba753a53202feeda942cfcf2e3a5a55639cd0c2f
WarEric <wareric@163.com>
2018-04-20 16:13:35 +0800

    Finish insert del leftRotate rightRotate. The next step is to test it.

--- 13/tree/RBTree.h
+++ 13/tree/RBTree.h
@@ -312,9 +312,8 @@ RBTNode<T>* RBTree<T>::copy(RBTNode<T> *root)
 template<typename T>
 RBTree<T>& RBTree<T>::operator=(const RBTree &orig)
 {
-	//maybe need run destroy
+	destroy();
 	root = copy(orig.root);
-	root->p = nil;
 }
 
 template<typename T>
@@ -361,7 +360,6 @@ bool RBTree<T>::operator==(const RBTree &dst)
 	//both equal to zero
 	if(stkd.size() != stks.size())
 		return false;
-
 	return true;
 }
 
@@ -369,6 +367,235 @@ template<typename T>
 bool RBTree<T>::operator!=(const RBTree &tree)
 {
 	return !(*this == tree);
+}
+
+template<typename T>
+bool RBTree<T>::insert(RBTNode<T> *z)
+{
+	RBTNode<T> *y = nil;
+	RBTNode<T> *x = root;
+	while(x != nil)
+	{
+		y = x;
+		if(z->getKey() < x->getKey())
+			x = x->l;
+		else
+			x = x->r;
+	}
+	z->parent = y;
+	if(y == nil)
+		root = z;
+	else if(z->getKey() < y->getKey())
+		y->l = z;
+	else
+		y->r = z;
+	z->l = nil;
+	z->r = nil;
+	z->setColor(RED);
+	insertFixUp(z);
+	return true;
+}
+
+template<typename T>
+void RBTree<T>::insertFixUp(RBTNode<T> *z)
+{
+	while(z->p->getColor() == RED)
+	{
+		if(z->p == z->p->p->l)
+		{
+			RBTNode<T> *y = z->p->p->r;
+			if(y->getColor() == RED)				//case 1
+			{
+				z->p->setColor(BLACK);
+				y->setColor(BLACK);
+				z->p->p->setColor(RED);
+				z = z->p->p;
+			}else{
+				if(z == z->p->r)				//case 2
+				{
+					z = z->p;
+					leftRotate(z);
+				}
+				z->p->setColor(BLACK);				//case 3
+				z->p->p->setColor(RED);
+				rightRotate(z->p->p);
+			}
+		}else{									
+			RBTNode<T> *y = z->p->p->l;
+			if(y->getColor() == RED)				//case 1
+			{
+				z->p->setColor(BLACK);
+				y->setColor(BLACK);
+				z->p->p->setColor(RED);
+				z = z->p->p;
+			}else{
+				if(z == z->p->l)				//case 2
+				{
+					z = z->p;
+					rightRotate(z);
+				}
+				z->p->setColor(BLACK);				//case 3
+				z->p->p->setColor(RED);
+				leftRotate(z->p->p);
+			}
+		}
+
+	}
+	root->setColor(BLACK);
+}
+
+template<typename T>
+bool RBTree<T>::del(RBTNode<T> *z)
+{
+	RBTNode<T> *y = z;
+	RBTNode<T> *x;
+	Color y_original_color = y->getColor();
+	if(z->l == nil)
+	{
+		x = z->r;
+		transplant(z, z->r);	
+	}else if(z->r == nil){
+		x = z->l;
+		transplant(z, z->l);	
+	}else{
+		y = min(z->r);	
+		y_original_color = y->getColor();
+		x = y->r;
+		if(y->p == z)
+		{	
+			x->p = y;
+		}else{
+			transplant(y, y->r);
+			y->r = z->r;
+			y->r->p = y;
+		}
+		transplant(z, y);
+		y->l = z->l;
+		y->l->p = y;
+		y->setColor(z->getColor());
+	}
+	delete z;
+
+	if(y_original_color == BLACK)
+		delFixUp(x);
+}
+
+template<typename T>
+void RBTree<T>::delFixUp(RBTNode<T> *x)
+{
+	while(x != root && x->getColor() == BLACK)
+	{
+		if(x == x->p->l)
+		{
+			RBTNode<T> *w = x->p->r;
+			if(w->getColor() == RED)				//case 1
+			{
+				w->setColor(BLACK);
+				x->p->setColor(RED);
+				leftRotate(x->p);
+				w = x->p->r;
+			}
+
+			if(w->l->getColor() == BLACK 
+					&& w->r->getColor() == BLACK)		//case 2
+			{
+				w->setColor(RED);
+				x = x->p;
+			}else{
+				if(w->r->getColor() == BLACK)			//case 3
+				{
+					w->l->setColor(BLACK);
+					w->setColor(RED);
+					rightRotate(w);
+					w = x->p->r;
+				}
+				w->setColor(x->p->getColor());			//case 4
+				x->p->setColor(BLACK);
+				w->r->setColor(BLACK);
+				leftRotate(x->p);
+				x = root;
+			}
+		}else{
+			RBTNode<T> *w = x->p->l;
+			if(w->getColor() == RED)				//case 1
+			{
+				w->setColor(BLACK);
+				x->p->setColor(RED);
+				rightRotate(x->p);
+				w = x->p->l;
+			}
+
+			if(w->l->getColor() == BLACK 
+					&& w->r->getColor() == BLACK)		//case 2
+			{
+				w->setColor(RED);
+				x = x->p;
+			}else{
+				if(w->l->getColor() == BLACK)			//case 3
+				{
+					w->r->setColor(BLACK);
+					w->setColor(RED);
+					leftRotate(w);
+					w = x->p->l;
+				}
+				w->setColor(x->p->getColor());			//case 4
+				x->p->setColor(BLACK);
+				w->l->setColor(BLACK);
+				rightRotate(x->p);
+				x = root;
+			}
+		}
+	}
+	x->setColor(BLACK);
+}
+
+template<typename T>
+void RBTree<T>::transplant(RBTNode<T> *u, RBTNode<T> *v)
+{
+	if(u->p == nil)
+		root = v;
+	else if(u == u->p->l)
+		u->p->l = v;
+	else
+		u->p->r = v;
+	v->p = u->p;
+}
+
+
+template<typename T>
+void RBTree<T>::leftRotate(RBTNode<T> *x)
+{
+	RBTNode<T> *y = x->r;
+	x->r = y->l;
+	if(y->l != nil)
+		y->l->p = x;
+	y->p = x->p;
+	if(x->p == nil)
+		root = y;
+	else if(x == x->p->l)
+		x->p->l = y;
+	else
+		x->p->r = y;
+	y->l = x;
+	x->p = y;
+}
+
+template<typename T>
+void RBTree<T>::rightRotate(RBTNode<T> *x)
+{
+	RBTNode<T> *y = x->l;
+	x->l = y->r;
+	if(y->r != nil)
+		y->r->p = x;
+	y->p = x->p;
+	if(x->p == nil)
+		root = y;
+	else if(x == x->p->r)
+		x->p->r = y;
+	else
+		x->p->l = y;
+	y->r = x;
+	x->p = y;
 }
 
 #endif
