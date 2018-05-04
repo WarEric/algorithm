3474878d0939fab638d87460392b610370d78a37
WarEric <wareric@163.com>
2018-05-04 16:04:01 +0800

    add interval tree header file

---
+++ 14/ITVNode.h
@@ -0,0 +1,9 @@
+/*
+ * 区间树的头文件，还是对红黑树数据结构的一种扩张
+ * by wareric@163.com
+ * 2018-05-04
+ */
+#ifndef ITVNODE_H_
+#define ITVNODE_H_
+
+#endif
---
+++ 14/ITVTree.h
@@ -0,0 +1,10 @@
+/*
+ * 区间树interval tree的头文件，这是对红黑树数据结构的
+ * 一种扩张
+ * by wareric@163.com
+ * 2018-05-04
+ */
+#ifndef ITVTREE_H_
+#define ITVTREE_H_
+
+#endif
